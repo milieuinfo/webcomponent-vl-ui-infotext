47b79d3927a6e94c1c5c76173fa7f2df1bcd362b
wautergu <guy.wauters@vlaanderen.be>
2020-02-06 09:33:25 +0100

    TK-502

--- test/e2e/infotext.test.js
+++ test/e2e/infotext.test.js
@@ -9,6 +9,10 @@ describe('vl-infotext', async () => {
         return vlInfotextPage.load();
     });
 
+    after(async () => { 
+        return driver.quit();
+    });
+
     it('als gebruiker kan ik de value opvragen', async () => {
         const infoText = await vlInfotextPage.getInfotext();
         await assert.eventually.equal(infoText.getValue(), '3200');
