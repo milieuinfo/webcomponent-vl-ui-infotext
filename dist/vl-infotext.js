f5c639be7d4a904d325ad993c397261210cd9fec
coemans <tom.coemans@gmail.com>
2020-07-16 16:47:49 +0200

    update dependencies (#41)

--- dist/vl-infotext.js
+++ dist/vl-infotext.js
@@ -8,7 +8,6 @@ import '/node_modules/@govflanders/vl-ui-infotext/dist/js/infotext.js';
  * @classdesc Gebruik de infotext om belangrijke getallen weer te geven.
  *
  * @extends HTMLDivElement
- * @mixin nativeVlElement
  *
  * @property {boolean} data-vl-badge - Attribuut wordt gebruikt om een badge te maken. Indien weggelaten wordt er geen badge gemaakt.
  *
